66798cd7fde70d2f6e9f05b8e6461f8b739b5eea
Joel <juniorm2421@gmail.com>
2024-06-05 16:43:44 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <?php include "bootstrap.head.php"; ?>
+</head>
+<body>
+
+<div class="jumbotron text-center">
+  <h1>Companies Investments</h1>
+  <p></p> 
+</div>
+  
+<div class="container">
+  <div class="row">
+    <div class="col-sm-4">
+      <h3>Report 1 <a href="report-01.php"> goto </a></h3>
+      <p>Allianz Life Insurance Co Of North America</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 2 <a href="report-02.php"> goto </a></h3>
+      <p>Berkshire Hathaway Inc</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 3 <a href="report-03.php"> goto </a></h3>        
+      <p>Bill  Melinda Gates Foundation Trust</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 4 <a href="report-04.php"> goto </a></h3>        
+      <p>BlackRock Inc</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 5 <a href="report-05.php"> goto </a></h3>        
+      <p>Citigroup Inc</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 6 <a href="report-06.php"> goto </a></h3>        
+      <p>Goldman Sachs Group</p>
+    </div>
+    <div class="col-sm-4">
+      <h3>Report 7 <a href="report-07.php"> goto </a></h3>        
+      <p>Td Ameritrade Investment Management LLC</p>
+    </div>
+  </div>
+</div>
+
+</body>
+</html>
---
+++ report-04.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Manager/Developer Mejia</title>
+  <?php include "bootstrap.head.php"; 
+        include "./utility/DBUtility.php";
+        include "./utility/Format.php";
+        $dbUtility = new DBUtility();
+        $sql = "Select id, symbol, issuerName, sharesValue
+                From investments 
+                Where companyId = 'B004' AND sharesValue > 5
+                Order by issuerName
+                limit 50";
+        $sql2 = "select id, company_name from company_name where id = 'B004'";
+        $rows = $dbUtility->execute($sql);
+        $rowWCompanyName = $dbUtility->execute($sql2);
+        $manager = $rowWCompanyName[0]["company_name"];
+  ?>
+</head>
+<body>
+
+<div class="container">
+  <h2>Managed by <?= "$manager" ?> <a href="./"> goto index </a></h2>
+  <p>A list of companies that are worth more than 5 billion dollars</p>            
+  <table class="table">
+    <thead>
+      <tr>
+        <th>id</th>
+        <th>symbol</th>
+        <th>issuer name</th>
+        <th>shares value</th>
+      </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($rows as $currentRow) { ?>
+            <tr>
+                <td><?= $currentRow["id"] ?></td>
+                <td><?= $currentRow["symbol"] ?></td>
+                <td><?= $currentRow["issuerName"] ?></td>
+                <td class='money'><?= money($currentRow["sharesValue"]) ?></td>
+            </tr>   
+     <?php } ?>
+    </tbody>
+  </table>
+</div>
+</body>
+</html>
